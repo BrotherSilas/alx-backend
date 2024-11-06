5f760d8ed22c8caa91a9fe1bc32ac3b68837412b
Silas Edet <silasedetsnr@gmail.com>
2024-11-06 03:00:08 +0000

    0x02-i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -4,14 +4,17 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -21,11 +24,12 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Route for the index page"""
     return render_template('4-index.html')
 
+
 if __name__ == "__main__":
     app.run()
-
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,11 +4,13 @@ from flask_babel import Babel, _
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 babel = Babel(app)
@@ -20,6 +22,7 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user():
     """Get the user dictionary if user ID is valid, otherwise return None"""
     user_id = request.args.get('login_as')
@@ -27,10 +30,12 @@ def get_user():
         return users.get(int(user_id))
     return None
 
+
 @app.before_request
 def before_request():
     """Set the user in the global request context before each request"""
     g.user = get_user()
+
 
 @babel.localeselector
 def get_locale():
@@ -42,11 +47,12 @@ def get_locale():
         return g.user["locale"]
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Route for the index page"""
     return render_template('5-index.html')
 
+
 if __name__ == "__main__":
     app.run()
-
