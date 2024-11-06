0d7de16e7699438efecd06402244185231559b03
Silas Edet <silasedetsnr@gmail.com>
2024-11-06 03:05:59 +0000

    0x02-i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python3
+"""Flask app with forced locale"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 
-
 class Config:
-    """Configuration for Babel"""
+    """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
 babel = Babel(app)
 
-
 @babel.localeselector
 def get_locale():
-    """Determine the best match for supported languages."""
+    """Get locale from request"""
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
 @app.route('/')
 def index():
-    """Route for the index page"""
+    """Basic route with translations"""
     return render_template('4-index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__ == "__main__":
-    app.run()
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
+"""Flask app with mock login"""
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
-
-
-class Config:
-    """Configuration for Babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -22,37 +12,40 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 def get_user():
-    """Get the user dictionary if user ID is valid, otherwise return None"""
-    user_id = request.args.get('login_as')
-    if user_id and int(user_id) in users:
-        return users.get(int(user_id))
+    """Returns user dict if ID exists"""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
     return None
-
 
 @app.before_request
 def before_request():
-    """Set the user in the global request context before each request"""
+    """Sets user as global on flask.g.user"""
     g.user = get_user()
-
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match for supported languages."""
+    """Get locale from request"""
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
-    if g.user and g.user.get("locale") in app.config["LANGUAGES"]:
-        return g.user["locale"]
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 @app.route('/')
 def index():
-    """Route for the index page"""
+    """Home page"""
     return render_template('5-index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__ == "__main__":
-    app.run()
