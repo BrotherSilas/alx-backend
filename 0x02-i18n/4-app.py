e490e64dd69fb7d2ff6abd69911997beede44b1b
Silas Edet <silasedetsnr@gmail.com>
2024-11-09 04:08:43 +0000

    0x02-i18n edit

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
-"""Flask app with forced locale"""
+"""Flask app with internationalization support"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
-app = Flask(__name__)
+from flask_babel import Babel
 
 
 class Config:
-    """Config class"""
+    """Flask Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Get locale from request"""
+def get_locale() -> str:
+    """Determine best match for supported languages.
+    First check for URL parameters, then resort to default behavior"""
+    # Check if locale parameter is in URL
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
+    
+    # Resort to default behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
-    """Basic route with translations"""
+def index() -> str:
+    """Render the index page"""
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
